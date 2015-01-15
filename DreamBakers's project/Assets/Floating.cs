7b9e65d26a9974abbb10352fc2c3b10312082472
Gabriel Simard <gabriel.simard.4@ulaval.ca>
2015-01-15 17:05:39 -0500

    Début du premier prototype
    Création d'une map test
    Création d'un personnage controlable au clavier
    Création de pilule flottant

---
+++ DreamBakers's project/Assets/Floating.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Floating : MonoBehaviour
+	{
+		public float intensiteDeRotation;
+
+		void Update ()
+		{
+			transform.Rotate(Time.deltaTime,intensiteDeRotation,intensiteDeRotation);
+		}
+	}
