7f096222c81dae41bcc83547d0e572b4583eb437
David <david.arel@outlook.com>
2015-01-18 22:56:07 -0500

    Logique du inGameMenu

---
+++ Unity project/Assets/InGameMenuController.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class InGameMenuController : MonoBehaviour {
+
+    private bool isPaused = false;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        checkIfInGameMenuIsTriggered();
+	}
+
+    private void checkIfInGameMenuIsTriggered()
+    {
+        if (Input.GetKeyDown("escape") & !isPaused)
+        {
+            print("Paused");
+            isPaused = true;
+        }
+        else if (Input.GetKeyDown("escape") & isPaused)
+        {
+            print("Unpaused");
+            isPaused = false;
+        }
+    }
+}
