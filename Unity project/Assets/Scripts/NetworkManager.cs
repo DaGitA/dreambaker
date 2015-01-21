3a5ab62575e8da1ea287e6d3f1abc18b1c01ef5c
Viennor <adam.b.bolduc@gmail.com>
2015-01-21 06:58:50 -0500

    Ajout d'un bouton Start Server et d'un script qui démarre un server.

---
+++ Unity project/Assets/Scripts/NetworkManager.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NetworkManager : MonoBehaviour {
+
+	private const string gameTypeName = "DreamBakerGame";
+	private const string gameName = "CoolRoom";
+	private HostData[] hostList;
+
+	public void startServer(){
+		Network.InitializeServer (2, 25000, !Network.HavePublicAddress ());
+		MasterServer.RegisterHost (gameTypeName, gameName);
+	}
+
+	void OnServerInitialized(){
+		Debug.Log("Server Initialized");
+	}
+	
+	public void joinServer(HostData hostData){
+		Network.Connect (hostData);
+	}
+
+	void OnConnectedServer(){
+		Debug.Log ("Server Joined");
+	}
+
+	private void refreshHostList(){
+		MasterServer.RequestHostList (gameTypeName);
+	}
+
+	void OnMasterServerEvent(MasterServerEvent serverEvent){
+		Debug.Log(serverEvent.ToString());
+		if (serverEvent == MasterServerEvent.HostListReceived) {
+			hostList = MasterServer.PollHostList();
+		}
+	}
+
+	void Start(){
+		refreshHostList ();
+	}
+	
+
+
+}
