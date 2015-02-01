c7c8a07ac9588d1c755c09cddcad1c50ef57c682
David <david.arel@outlook.com>
2015-01-31 19:00:42 -0500

    Test de test unitaire et début de gestion de l'augmentation de la folie.

---
+++ Unity project/Assets/Scripts/GameController.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameController : MonoBehaviour {
+
+    float crazynessLevel;
+    float lastCrazynessLevelUpdate;
+    private float TIME_TO_RAISE_CRAYZYNESS_LEVEL = 3;
+
+    // Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        handleCrazynessLevel();
+  
+	}
+
+    private void handleCrazynessLevel()
+    {
+        if (Time.time - lastCrazynessLevelUpdate == TIME_TO_RAISE_CRAYZYNESS_LEVEL)
+        {
+            crazynessLevel += 1;
+            lastCrazynessLevelUpdate = Time.time;
+        }    
+    }
+}
