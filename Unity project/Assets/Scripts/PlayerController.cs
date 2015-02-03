9d45062327ebb0c3080d38c1baf0747a5d0fde36
brcc0 <brunoccoulombe@gmail.com>
2015-02-03 15:43:38 -0500

    Ajout du player simplifié dans la scène et retrait du construction worker

---
+++ Unity project/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+    public float MoveSpeed = 5;
+    public float RotateSpeed = 80;
+    
+    // Use this for initialization    
+    void Start() {
+    }
+
+    // Update is called once per frame
+    void Update() {
+        float MoveForward = Input.GetAxis("Vertical") * MoveSpeed * Time.deltaTime;
+        float MoveRotate = Input.GetAxis("Horizontal") * RotateSpeed * Time.deltaTime;
+        transform.Translate(Vector3.forward * MoveForward);
+        transform.Rotate(Vector3.up * MoveRotate);
+	}
+}
