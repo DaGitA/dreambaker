92b9d6bda344fe630d781e6c6a41431576637fef
Viennor <adam.b.bolduc@gmail.com>
2015-02-04 15:03:23 -0500

    Creation d'une antichambre ou les joueurs vont attendre le debut de la partie.

--- Unity project/Assets/Scripts/ServerController.cs
+++ Unity project/Assets/Scripts/ServerController.cs
@@ -55,7 +55,6 @@ public class ServerController : MonoBehaviour {
 
     void OnPlayerDisconnected(NetworkPlayer player) {
         players.Remove(player);
-        Debug.LogError("YARHH");
     }
 
     
