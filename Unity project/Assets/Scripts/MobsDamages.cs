e77c9f996b63017dac15672131a8dc9620a80265
David <courriel_878@outlook.com>
2015-02-09 13:35:21 -0500

    Ajout du script pour que les mobs puissent perdre de la vie.

---
+++ Unity project/Assets/Scripts/MobsDamages.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+using Assets.Scripts;
+
+public class MobsDamages : MonoBehaviour {
+
+    public int mobLife;
+
+	// Use this for initialization
+	void Start () {
+        
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    private void takeDamage(Attack attackOnMob){
+        mobLife = mobLife - attackOnMob.damage;
+    }
+
+}
