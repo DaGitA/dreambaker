1841583e0d877a4b6ea357bd0d491723866785e5
David <courriel_878@outlook.com>
2015-02-09 16:33:39 -0500

    Checkpoint done

--- Unity project/Assets/Scripts/GameController.cs
+++ Unity project/Assets/Scripts/GameController.cs
@@ -3,12 +3,14 @@ using System.Collections;
 
 public class GameController : MonoBehaviour {
 
+    private static Vector3 START_LOCATION = new Vector3(0,0,0);
     private float crazynessLevel;
     public UnityEngine.UI.Text crazynessLevelText;
     public UnityEngine.UI.Slider crazynessLevelSlider;
     float lastCrazynessLevelUpdate;
     private float TIME_TO_RAISE_CRAYZYNESS_LEVEL = 3;
     private int DECREASE_CRAZINESS_LEVEL_WITH_PILL = 10;
+    private Vector3 nextRespawnLocation = START_LOCATION;
 
     // Use this for initialization
 	void Start () {
@@ -49,5 +51,10 @@ public class GameController : MonoBehaviour {
     {
         crazynessLevelText.text = crazynessLevel.ToString();
         crazynessLevelSlider.value = crazynessLevel;
+    }
+
+    public void setNextRespawnLocation(Vector3 checkpointPosition)
+    {
+        nextRespawnLocation = checkpointPosition;
     }
 }
