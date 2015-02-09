b8d7e41d25880bc7a13148c4651a294429bdf6fb
camillebeland <camillebeland1@gmail.com>
2015-02-09 14:06:50 -0500

    premier essai pour merger la branche a david

---
+++ Unity project/Assets/Scripts/Attack.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Attack : MonoBehaviour
+{
+    private int damage;
+    public float DAMAGE_STRENGTH = 0.05f;
+
+    public int getDamage()
+    {
+        return damage;
+    }
+    
+    public void calculateDamage() 
+    {
+        damage += damage * ((int) DAMAGE_STRENGTH);
+    }
+}
---
+++ Unity project/Assets/Scripts/FightController.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FightController : MonoBehaviour
+{
+    public float attackTime, waitTime = 1.0f;
+    protected bool canAttack = true;
+    public float DAMAGE_STRENGTH = 0.05f;
+    private Collider ennemyCollider;
+
+    public void OnMouseDown()
+    {
+        attackTime = Time.time;
+        if (canAttack)
+        {
+            damage += damage * ((int)DAMAGE_STRENGTH);
+            canAttack = false;
+        }
+    }
+
+    public void Update()
+    {
+        if (!canAttack)
+        {
+            if (Time.time > attackTime + waitTime)
+                canAttack = true;
+        }
+    }
+}
