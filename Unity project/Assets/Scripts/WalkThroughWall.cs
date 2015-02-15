b37a424b891caf2da3aa4115259404e8e9ca2847
brcc0 <brunoccoulombe@gmail.com>
2015-02-15 12:04:34 -0500

    Puzzle simple

---
+++ Unity project/Assets/Scripts/WalkThroughWall.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+public class WalkThroughWall : MonoBehaviour
+{
+    public GameObject wall;
+
+    private void Start()
+    {
+    }
+
+    private void Update()
+    {
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.name == "Player")
+        {
+            Physics.IgnoreCollision(wall.collider, other.collider);
+        }
+    }
+}
