8291183d474f563ede6102cc3ab883c8e948b4db
camillebeland <camillebeland1@gmail.com>
2015-03-03 12:48:28 -0500

    attaques avec timer

--- Unity project/Assets/Scripts/PlayerAttackBehaviour.cs
+++ Unity project/Assets/Scripts/PlayerAttackBehaviour.cs
@@ -4,11 +4,16 @@ using System.Collections;
 public class PlayerAttackBehaviour : MonoBehaviour {
 
     public WeaponAttackBehaviour weaponPrefab;
+    private Timer timer;
     public bool isOnCoolDown = false;
+    public float attackTime = 0.2f;
 
     void Start()
     {
         weaponPrefab = this.GetComponentInChildren<WeaponAttackBehaviour>();
+        timer = gameObject.AddComponent<Timer>();
+        timer.trigger = this;
+        timer.timerValue = attackTime;
     }
 
     void Update()
@@ -16,13 +21,16 @@ public class PlayerAttackBehaviour : MonoBehaviour {
         if (Input.GetButton("Fire2") && !isOnCoolDown)
         {
             weaponPrefab.gameObject.SetActive(true);
+            timer.startTimer();
+            isOnCoolDown = true;
         }
         
     }
 
-    void FixedUpdate()
+    void timesUp()
     {
         weaponPrefab.gameObject.SetActive(false);
+        isOnCoolDown = false;
     }
 
     private void attack()
