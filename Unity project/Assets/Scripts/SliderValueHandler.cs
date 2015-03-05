26ca712b6b2656202cfbcd7a272326c89a28e4d1
David <courriel_878@outlook.com>
2015-03-05 15:48:42 -0500

    Le crazynessLevelText suis le slider et le HUD n'apparait par dessus le menu au début

--- Unity project/Assets/Scripts/SliderValueHandler.cs
+++ Unity project/Assets/Scripts/SliderValueHandler.cs
@@ -2,13 +2,18 @@
 using System.Collections;
 
 public class SliderValueHandler : MonoBehaviour {
-
+    private UnityEngine.UI.Slider crazinessLevelSlider;
+    private UnityEngine.UI.Text crazinessLevelText;
 
     void Start()
     {
+        //this.OnValueChanged();
+        //gameObject .onValueChanged.AddListener(ListenerMethod);
+        crazinessLevelSlider = GameObject.Find("CrazynessSlider").GetComponent<UnityEngine.UI.Slider>();
+        crazinessLevelText = GameObject.FindGameObjectWithTag("crazynessLevelText").GetComponent<UnityEngine.UI.Text>();
     }
 
-    void OnValueChange(float newValue){
-        
+    public void changeCrazinessText(){
+        crazinessLevelText.text = crazinessLevelSlider.value.ToString();
     }
 }
