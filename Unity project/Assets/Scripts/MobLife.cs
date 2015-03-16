425bb618662437fcef9a0a367344ad7bca85e3db
David <courriel_878@outlook.com>
2015-03-16 12:58:55 -0400

    Bug du start du timer réglé. L'anim du mob hit s'arrete. C'est hardcodé dans moblife. Le mieux serait d'avoir une anim sans loop.

--- Unity project/Assets/Scripts/MobLife.cs
+++ Unity project/Assets/Scripts/MobLife.cs
@@ -6,16 +6,22 @@ public class MobLife : MonoBehaviour {
     public float healthBar = 100;
     private Animator animator;
     private GameObject gameController;
+    private Timer myTimer;
 
     void Awake()
     {
         animator = this.GetComponentInChildren<Animator>();
         gameController = GameObject.FindWithTag("GameController");
+        myTimer = gameObject.AddComponent<Timer>();
+        myTimer.trigger = this;
+        float hitAnimationDelay = 1.0f;
+        myTimer.timerValue = hitAnimationDelay;
     }
 
     public void takeDamage(float attackDamage)
     {
         animator.SetBool("isHit", true);
+        myTimer.startTimer();
         healthBar -= attackDamage;
         Debug.Log(healthBar);
         if (healthBar <= 0)
@@ -28,5 +34,11 @@ public class MobLife : MonoBehaviour {
     {
         animator.SetBool("isDead", true);
         gameController.SendMessage("mort", gameObject);
+    }
+
+    public void timesUp()
+    {
+        animator.SetBool("isHit", false);
+        Debug.Log("timesup");
     }
 }
